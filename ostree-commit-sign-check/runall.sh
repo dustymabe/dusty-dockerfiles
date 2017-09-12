3321a31a2a6563e3b4822045d602b29a99e081f5
Dusty Mabe <dusty@dustymabe.com>
2017-09-12 17:00:51 -0400

    ostree checker: add other architectures

--- ostree-commit-sign-check/runall.sh
+++ ostree-commit-sign-check/runall.sh
@@ -1,9 +1,24 @@
 
 dir=$(dirname $0)
 
+# f26
 bash $dir/run.sh 26 fedora/26/x86_64/updates/atomic-host
 bash $dir/run.sh 26 fedora/26/x86_64/testing/atomic-host
-bash $dir/run.sh 27 fedora/27/x86_64/atomic-host
-#bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
-#bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host
+
+# f27 (no updates or testing yet)
+bash $dir/run.sh 27 fedora/27/x86_64/atomic-host  https://kojipkgs.fedoraproject.org/compose/atomic/27/
+bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
+bash $dir/run.sh 27 fedora/27/aarch64/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
+
+# f27
+#   bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
+#   bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host
+#   bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host
+#   bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host
+#   bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host
+#   bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host
+
+# rawhide
 bash $dir/run.sh rawhide fedora/rawhide/x86_64/atomic-host
+bash $dir/run.sh rawhide fedora/rawhide/ppc64le/atomic-host
+bash $dir/run.sh rawhide fedora/rawhide/aarch64/atomic-host
