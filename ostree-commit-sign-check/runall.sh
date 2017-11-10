48369a94479486a2c131ec4cca5a8982e55abdb3
Dusty Mabe <dusty@dustymabe.com>
2017-11-10 14:12:12 -0500

    ostree commit checker: move f26 to bottom + f27-u-t

--- ostree-commit-sign-check/runall.sh
+++ ostree-commit-sign-check/runall.sh
@@ -1,11 +1,6 @@
 
 dir=$(dirname $0)
 
-# f26
-bash $dir/run.sh 26 fedora/26/x86_64/updates/atomic-host
-bash $dir/run.sh 26 fedora/26/x86_64/testing/atomic-host
-bash $dir/run.sh 26 fedora/26/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/26/
-
 # f27 (no updates or testing yet)
 bash $dir/run.sh 27 fedora/27/x86_64/atomic-host  https://kojipkgs.fedoraproject.org/compose/atomic/27/
 bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
@@ -16,7 +11,7 @@ bash $dir/run.sh 27 fedora/27/x86_64/workstation  https://kojipkgs.fedoraproject
 #   bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
 #   bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host
 #   bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host
-#   bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host
+bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
 #   bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host
 #   bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host
 #   bash $dir/run.sh 27 fedora/27/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/27/
@@ -26,3 +21,8 @@ bash $dir/run.sh rawhide fedora/rawhide/x86_64/atomic-host
 bash $dir/run.sh rawhide fedora/rawhide/ppc64le/atomic-host
 bash $dir/run.sh rawhide fedora/rawhide/aarch64/atomic-host
 bash $dir/run.sh rawhide fedora/rawhide/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/rawhide/
+
+# f26
+bash $dir/run.sh 26 fedora/26/x86_64/updates/atomic-host
+bash $dir/run.sh 26 fedora/26/x86_64/testing/atomic-host
+bash $dir/run.sh 26 fedora/26/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/26/
