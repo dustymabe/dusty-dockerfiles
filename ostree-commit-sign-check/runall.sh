ef16e664db82792b4be188a21ae1ac339f372012
Dusty Mabe <dusty@dustymabe.com>
2017-11-13 19:54:22 -0500

    ostree checker: f27 stuff is now in prod locations

--- ostree-commit-sign-check/runall.sh
+++ ostree-commit-sign-check/runall.sh
@@ -1,20 +1,20 @@
 
 dir=$(dirname $0)
 
-# f27 (no updates or testing yet)
-bash $dir/run.sh 27 fedora/27/x86_64/atomic-host  https://kojipkgs.fedoraproject.org/compose/atomic/27/
-bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
-bash $dir/run.sh 27 fedora/27/aarch64/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
-bash $dir/run.sh 27 fedora/27/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/27/
-
 # f27
-#   bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
-#   bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host
-#   bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host
-bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host https://kojipkgs.fedoraproject.org/compose/atomic/27/
-#   bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host
-#   bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host
-#   bash $dir/run.sh 27 fedora/27/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/27/
+bash $dir/run.sh 27 fedora/27/x86_64/atomic-host
+bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
+bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host
+
+bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host
+bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host
+bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host
+
+bash $dir/run.sh 27 fedora/27/aarch64/atomic-host
+bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host
+bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host
+
+bash $dir/run.sh 27 fedora/27/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/27/
 
 # rawhide
 bash $dir/run.sh rawhide fedora/rawhide/x86_64/atomic-host
