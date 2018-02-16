e381f36d8e064b302a973a566b481f29843997a9
Dusty Mabe <dusty@dustymabe.com>
2018-02-15 22:06:37 -0500

    failed-compose: enhance logging, add a few log statements

--- failed-composes/failed-compose-checker.py
+++ failed-composes/failed-compose-checker.py
@@ -4,10 +4,18 @@ import re
 import os
 from libpagure import Pagure
 
-# Set logging to not print warnings
+# Set fedmsg logging to not print warnings
 import logging
-log = logging.getLogger('fedmsg')
-log.setLevel(logging.ERROR)
+logger = logging.getLogger('fedmsg')
+logger.setLevel(logging.ERROR)
+
+# Set local logging 
+logger = logging.getLogger(__name__)
+sh = logging.StreamHandler()
+sh.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+logger.addHandler(sh)
+logger.setLevel(logging.INFO)
+
 
 # Connect to pagure and set it to point to our repo
 PAGURE_REPO='dusty/failed-composes'
@@ -37,29 +45,31 @@ def main():
     # grab token and connec to pagure
     token = os.getenv('PAGURE_TOKEN')
     if token:
+        logger.info("Using detected token to talk to pagure.") 
         pg = Pagure(pagure_token=token)
     else:
-        print("No token was detected.") 
-        print("This script will run but won't be able to create new issues.")
+        logger.info("No pagure token was detected.") 
+        logger.info("This script will run but won't be able to create new issues.")
         pg = Pagure()
 
     # Set the repo to create new issues against
     pg.repo=PAGURE_REPO
 
     # Grab messages from fedmsg and process them as we go
+    logger.info("Starting listening for fedmsgs..") 
     for name, endpoint, topic, msg in fedmsg.tail_messages():
         if "pungi.compose.status.change" in topic and msg['msg']['status'] != 'FINISHED':
 
             # We have a compose that either failed or had missing artifacts
             # create a new issue.
             title = msg['msg']['compose_id'] + ' ' + msg['msg']['status']
-            logfile = msg['msg']['location'] + '/../logs/global/pungi.global.log'
+            logfileurl = msg['msg']['location'] + '/../logs/global/pungi.global.log'
 
             # pull only part of the compose ID for the tag to set
             tag = re.search('(.*)-\d{8}', msg['msg']['compose_id']).group(1)
             #TODO figure out how to set tag on an issue
 
-            print("%s\t%s" % (title, logfile))
+            logger.info("%s\t%s" % (title, logfileurl))
             pg.create_issue(title=title, content=logfile)
 
 if __name__ == '__main__':
