ba5621e8264a7d743c6dccd65190fa847bc7420f
Dusty Mabe <dusty@dustymabe.com>
2018-02-21 15:43:40 -0500

    failed-compose: switch up print statement for debugging

--- failed-composes/failed-compose-checker.py
+++ failed-composes/failed-compose-checker.py
@@ -74,10 +74,9 @@ def main():
             date = today
             logger.info('mark')
 
-        print(topic) # some sort of indicator of progress
-
         if "pungi.compose.status.change" in topic:
             print('.', end='') # some sort of indicator of progress
+            print('.') # some sort of indicator of progress
 
             status = msg['msg']['status']
 
