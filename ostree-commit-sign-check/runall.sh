84e6a434613e8f03e296328b60f8dd438af99a10
Dusty Mabe <dusty@dustymabe.com>
2018-04-13 16:21:26 -0400

    ostree-commit-checker: update for unified repo changes

--- ostree-commit-sign-check/runall.sh
+++ ostree-commit-sign-check/runall.sh
@@ -1,30 +1,47 @@
 
 dir=$(dirname $0)
 
+# f28
+bash $dir/run.sh 28 fedora/28/x86_64/atomic-host
+#bash $dir/run.sh 28 fedora/28/x86_64/updates/atomic-host
+bash $dir/run.sh 28 fedora/28/x86_64/testing/atomic-host
+
+bash $dir/run.sh 28 fedora/28/ppc64le/atomic-host
+#bash $dir/run.sh 28 fedora/28/ppc64le/updates/atomic-host
+bash $dir/run.sh 28 fedora/28/ppc64le/testing/atomic-host
+
+bash $dir/run.sh 28 fedora/28/aarch64/atomic-host
+#bash $dir/run.sh 28 fedora/28/aarch64/updates/atomic-host
+bash $dir/run.sh 28 fedora/28/aarch64/testing/atomic-host
+
+bash $dir/run.sh 28 fedora/28/x86_64/workstation         
+#bash $dir/run.sh 28 fedora/28/x86_64/updates/workstation
+bash $dir/run.sh 28 fedora/28/x86_64/testing/workstation
+
 # f27
-bash $dir/run.sh 27 fedora/27/x86_64/atomic-host
-bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host
-bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host
+bash $dir/run.sh 27 fedora/27/x86_64/atomic-host          https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/x86_64/updates/atomic-host  https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/x86_64/testing/atomic-host  https://kojipkgs.fedoraproject.org/atomic/27/
 
-bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host
-bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host
-bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host
+bash $dir/run.sh 27 fedora/27/ppc64le/atomic-host         https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/ppc64le/updates/atomic-host https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/ppc64le/testing/atomic-host https://kojipkgs.fedoraproject.org/atomic/27/
 
-bash $dir/run.sh 27 fedora/27/aarch64/atomic-host
-bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host
-bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host
+bash $dir/run.sh 27 fedora/27/aarch64/atomic-host         https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/aarch64/updates/atomic-host https://kojipkgs.fedoraproject.org/atomic/27/
+bash $dir/run.sh 27 fedora/27/aarch64/testing/atomic-host https://kojipkgs.fedoraproject.org/atomic/27/
 
-bash $dir/run.sh 27 fedora/27/x86_64/workstation         https://dl.fedoraproject.org/ostree/27/
-bash $dir/run.sh 27 fedora/27/x86_64/updates/workstation https://dl.fedoraproject.org/ostree/27/
-bash $dir/run.sh 27 fedora/27/x86_64/testing/workstation https://dl.fedoraproject.org/ostree/27/
+bash $dir/run.sh 27 fedora/27/x86_64/workstation
+bash $dir/run.sh 27 fedora/27/x86_64/updates/workstation
+bash $dir/run.sh 27 fedora/27/x86_64/testing/workstation
 
 # rawhide
 bash $dir/run.sh rawhide fedora/rawhide/x86_64/atomic-host
 bash $dir/run.sh rawhide fedora/rawhide/ppc64le/atomic-host
 bash $dir/run.sh rawhide fedora/rawhide/aarch64/atomic-host
-bash $dir/run.sh rawhide fedora/rawhide/x86_64/workstation  https://kojipkgs.fedoraproject.org/compose/ostree/rawhide/
+bash $dir/run.sh rawhide fedora/rawhide/x86_64/workstation
 
 # f26
-bash $dir/run.sh 26 fedora/26/x86_64/atomic-host
-bash $dir/run.sh 26 fedora/26/x86_64/updates/atomic-host
-bash $dir/run.sh 26 fedora/26/x86_64/testing/atomic-host
+bash $dir/run.sh 26 fedora/26/x86_64/atomic-host         https://kojipkgs.fedoraproject.org/atomic/26/
+bash $dir/run.sh 26 fedora/26/x86_64/updates/atomic-host https://kojipkgs.fedoraproject.org/atomic/26/
+bash $dir/run.sh 26 fedora/26/x86_64/testing/atomic-host https://kojipkgs.fedoraproject.org/atomic/26/
